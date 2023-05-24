abace80edc9f08d2891de6ad41aa384c68b0dd47
Areay7 <2507549477@qq.com>
2023-05-24 16:45:50 +0800

    firsr commit

---
+++ src/include/AiOS/AiOS.h
@@ -0,0 +1,8 @@
+#ifndef AiOS_H
+#define AiOS_H
+
+#define AiOS_MAGIC 20230520
+
+void kernel_init(); // 初始化内核
+
+#endif
---
+++ test/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello ky !!! \n");
+    return 0;
+}
